5cdfaf08e510c605a124ebd419eb19d31ad29f48
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-03-26 16:40:14 -0400

    Updated date timer to be working and constantly updating

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,19 @@ var timeBlock = document.querySelector(".time-block");
 var currentDateEl = document.querySelector("#lead");
 var timerAndDateEl = moment().format("LLLL");
 
-// var currentDate = $(window).load(function () {
-//   //   onload(window).then(timerAndDateEl);
-//   var date = document.createElement("p");
-//   date.innerText = timerAndDateEl;
-//   currentDateEl.appendChild(date);
-// });
+var currentDate = function () {
+  //   onload(window).then(timerAndDateEl);
+  //   var date = document.createElement("p");
+  $("#date").text(moment().format("dddd, MMMM Do - h:mm:ss a"));
+  //   date.innerHTML = timerAndDateEl;
+};
+currentDate();
 
+var windowStart = function () {
+  setInterval(currentDate, 1000);
+};
+
+windowStart();
 // function setUpTimedElements() {
 //   updateDate();
 //   renderTextareaBackground();
@@ -17,14 +23,14 @@ var timerAndDateEl = moment().format("LLLL");
 // function updateDate() {
 //   $("#date").text(moment().format("dddd, MMMM Do - h:mm:ss a"));
 // }
-var currentDate = function () {
-  var today = new Date();
-  var date =
-    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-  var time =
-    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  var dateTime = date + " " + time;
-  var createDate = document.createElement("p");
-  date.innerText = timerAndDateEl;
-  currentDateEl.appendChild(createDate);
-};
+// var currentDate = function () {
+//   var today = new Date();
+//   var date =
+//     today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+//   var time =
+//     today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//   var dateTime = date + " " + time;
+//   var createDate = document.createElement("p");
+//   date.innerHTML = timerAndDateEl;
+//   currentDateEl.appendChild(createDate);
+// };

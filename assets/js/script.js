5972db5d8dc968f549c66058ce303e15dabce8f7
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-03-27 14:30:24 -0400

    Added functionality to the planner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,67 @@
-var timeBlock = document.querySelector(".time-block");
-var currentDateEl = document.querySelector("#lead");
-var timerAndDateEl = moment().format("LLLL");
+var currentDayEl = document.querySelector("#currentDay");
+var scheduleContainerEl = document.querySelector(".container");
 
-var currentDate = function () {
-  //   onload(window).then(timerAndDateEl);
-  //   var date = document.createElement("p");
+var textAreaEl = document.querySelectorAll(".description");
+
+var saveBtnEl = document.querySelectorAll(".saveBtn");
+
+var currentTime = Number(moment().format("H"));
+
+var workHours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+
+// displaying the day on top of the calender
+
+var displayDay = function () {
   $("#date").text(moment().format("dddd, MMMM Do - h:mm:ss a"));
-  //   date.innerHTML = timerAndDateEl;
 };
-currentDate();
+// displayDay();
 
-var windowStart = function () {
-  setInterval(currentDate, 1000);
+var checkTime = function () {
+  for (var i = 0; i < workHours.length; i++) {
+    if (currentTime > workHours[i]) {
+      textAreaEl[i].classList.add("past");
+    } else if (currentTime === workHours[i]) {
+      textAreaEl[i].classList.add("present");
+    } else {
+      textAreaEl[i].classList.add("future");
+    }
+  }
+};
+// checkTime();
+
+//local storage
+
+var saveDescription = function (event) {
+  var time = event.target.parentNode.children[0].textContent;
+  var description = event.target.parentNode.children[1].value;
+
+  localStorage.setItem(time, description);
 };
 
-windowStart();
-// function setUpTimedElements() {
-//   updateDate();
-//   renderTextareaBackground();
-// }
+var getSchedule = function () {
+  for (var i = 0; i < textAreaEl.length; i++) {
+    var time = textAreaEl[i].parentNode.children[0].textContent;
 
-// function updateDate() {
-//   $("#date").text(moment().format("dddd, MMMM Do - h:mm:ss a"));
-// }
-// var currentDate = function () {
-//   var today = new Date();
-//   var date =
-//     today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-//   var time =
-//     today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-//   var dateTime = date + " " + time;
-//   var createDate = document.createElement("p");
-//   date.innerHTML = timerAndDateEl;
-//   currentDateEl.appendChild(createDate);
-// };
+    var saveValue = localStorage.getItem(time);
+    textAreaEl[i].textContent = saveValue;
+  }
+};
+// getSchedule();
+
+//event
+var addEvents = function () {
+  for (var i = 0; i < saveBtnEl.length; i++) {
+    saveBtnEl[i].addEventListener("click", saveDescription);
+  }
+};
+addEvents();
+
+//When document is ready, set up
+$(document).ready(function () {
+  displayDay();
+  getSchedule();
+  checkTime();
+  // Check time sensitive elements using intervals
+  setInterval(displayDay, 1000);
+  setInterval(checkTime, 60 * 1000);
+});
